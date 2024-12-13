9f21f9cab2cb05fb29c03e837fe5b1f5a28b0d8e
Nolan Moak <nolanmoak@live.com>
2024-12-13 14:39:30 -0500

    Added react-native-reusables and nativewind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,78 @@
-import { Stack } from "expo-router";
+import '~/globals.css';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { DefaultTheme, Theme, ThemeProvider } from '@react-navigation/native';
+import { PortalHost } from '@rn-primitives/portal';
+import { Slot, SplashScreen } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { Platform } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { NAV_THEME } from '~/constants/nav-theme-colors';
+import { useColorScheme } from '~/hooks/use-color-scheme';
+import { setAndroidNavigationBar } from '~/lib/android-navigation-bar';
+
+const LIGHT_THEME: Theme = {
+  dark: false,
+  colors: NAV_THEME.light,
+  fonts: DefaultTheme.fonts,
+};
+const DARK_THEME: Theme = {
+  dark: true,
+  colors: NAV_THEME.dark,
+  fonts: DefaultTheme.fonts,
+};
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
+// Prevent the splash screen from auto-hiding before getting the color scheme.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    (async () => {
+      const theme = await AsyncStorage.getItem('theme');
+      if (Platform.OS === 'web') {
+        // Adds the background color to the html element to prevent white background on overscroll.
+        document.documentElement.classList.add('bg-background');
+      }
+      if (!theme) {
+        setAndroidNavigationBar(colorScheme);
+        AsyncStorage.setItem('theme', colorScheme);
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      const colorTheme = theme === 'dark' ? 'dark' : 'light';
+      setAndroidNavigationBar(colorScheme);
+      if (colorTheme !== colorScheme) {
+        setColorScheme(colorTheme);
+
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      setIsColorSchemeLoaded(true);
+    })().finally(() => {
+      SplashScreen.hideAsync();
+    });
+  }, [colorScheme, setColorScheme]);
+
+  if (!isColorSchemeLoaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+      <SafeAreaView className='bg-background flex-1'>
+        <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
+        <Slot />
+        <PortalHost />
+      </SafeAreaView>
+    </ThemeProvider>
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,13 @@
-import { Text, View } from "react-native";
+import { View } from 'react-native';
+import { Text } from '~/components/ui/text';
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
